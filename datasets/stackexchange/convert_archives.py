60d6d3e10b225ed35485ca516b42baf40c721116
mandubian <pascal.voitot.dev@gmail.com>
2021-07-01 18:47:49 +0200

    adding poetry support to manage dependencies & tooling

--- datasets/stackexchange/convert_archives.py
+++ datasets/stackexchange/convert_archives.py
@@ -11,18 +11,16 @@ import os
 import glob
 import sys
 
-sys.path.append("../..")
-
 from datasets.stackexchange.convert import convert_to_jsonl_gz
 
-input_path_files = os.path.join("archive", "*.7z")
+input_path_files = os.path.join("data/archive", "*.7z")
 
 files = glob.glob(input_path_files)
 
-output_path = "jsonl"
+output_path = "data/jsonl"
 os.makedirs(output_path, exist_ok=True)
 
 for f in files:
-    name = f.strip(".7z")
+    name = os.path.basename(f).strip(".7z")
     print(name)
     convert_to_jsonl_gz(f, os.path.join(output_path, f"{name}.jsonl.gz"))
