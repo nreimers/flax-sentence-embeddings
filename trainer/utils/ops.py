941d680177b2a65fcd86f43e11ef0efcce2ae280
vinid <chiccobia@gmail.com>
2021-07-01 17:47:18 +0200

    adding mean pooling

--- trainer/utils/ops.py
+++ trainer/utils/ops.py
@@ -18,5 +18,5 @@ def mean_pooling(model_output, attention_mask):
     :param attention_mask: the attention mask to zero out some vectors
     :return: the mean pooled embeddings
     """
-    input_mask_expanded = jnp.repeat(jnp.expand_dims(attention_mask, -1), (model_output).shape[2], axis=2)
+    input_mask_expanded = jnp.repeat(jnp.expand_dims(attention_mask, -1), model_output.shape[2], axis=2)
     return jnp.sum(model_output * input_mask_expanded, 1) / jnp.clip(input_mask_expanded.sum(1), a_min=1e-9)
