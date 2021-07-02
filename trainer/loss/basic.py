ab415d4777452c455b1508e23b25f3fd0c4018c4
Trent <devtrentoh@gmail.com>
2021-07-01 23:59:52 -0700

    MultipleNegativesRankingLoss with hard negative support

--- trainer/loss/basic.py
+++ trainer/loss/basic.py
@@ -4,5 +4,10 @@ import optax
 
 
 @jax.jit
-def cross_entropy_loss(scores, labels):
-    return jnp.mean(optax.softmax_cross_entropy(scores, jax.nn.one_hot(labels, num_classes=len(labels))))
+def padded_cross_entropy_loss(scores, labels):
+    class_count = len(labels)
+    assert scores.shape[0] >= class_count
+    labels = jnp.pad(labels, (0, scores.shape[0] - class_count), constant_values=0)
+    one_hot = jax.nn.one_hot(labels, num_classes=class_count)
+    soft_max = optax.softmax_cross_entropy(scores, one_hot)
+    return jnp.mean(soft_max)
