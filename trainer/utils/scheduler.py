897983a44900620963d9de51e07df1b22604cd2a
zanussbaum <zanussbaum@gmail.com>
2021-07-07 22:53:44 -0400

    Adding linear schedule w/ test

--- trainer/utils/scheduler.py
+++ trainer/utils/scheduler.py
@@ -1,7 +1,7 @@
 import optax
 
 
-def lr_schedule(lr, init_lr=None, warmup_steps=None, warmup=False):
+def constant_lr_schedule(lr, init_lr=None, warmup_steps=None, warmup=False):
     """Creates a constant LR Schedule with optional warmup of `warmup_steps`
 
     Args:
@@ -20,12 +20,48 @@ def lr_schedule(lr, init_lr=None, warmup_steps=None, warmup=False):
     schedule = optax.constant_schedule(value=lr)
 
     if warmup:
-        warmup_schedule = optax.linear_schedule(
-            init_value=init_lr, end_value=lr, transition_steps=warmup_steps
+        schedule = warmup_schedule(init_lr, lr, warmup_steps, schedule)
+
+    return schedule
+
+
+def linear_lr_schedule(lr, end_lr, num_train_steps, init_lr=None, warmup_steps=None, warmup=False):
+    """Creates a linear LR Schedule with optional warmup of `warmup_steps`
+
+    Args:
+        lr: an end value for the learning rate during the linear schedule
+        init_lr: an initial value for the learning rate during the linear schedule
+        warmup_steps: number of steps for the linear schedule
+        warmup: if we should use a linear warmup schedule
+
+    Returns:
+        optax.schedule that maps steps to LR
+
+    """
+    if warmup and (warmup_steps is None or init_lr is None):
+        raise ValueError("Must provide warmup steps of warmup is set to True")
+
+    if warmup:
+        decay_steps = num_train_steps - warmup_steps
+        schedule = optax.linear_schedule(
+            init_value=lr, end_value=end_lr, transition_steps=decay_steps
+        )
+        schedule = warmup_schedule(init_lr, lr, warmup_steps, schedule)
+    else:
+        schedule = optax.linear_schedule(
+            init_value=lr, end_value=end_lr, transition_steps=num_train_steps
         )
 
-        schedule = optax.join_schedules(
-            schedules=[warmup_schedule, schedule], boundaries=[warmup_steps]
-        )
+    return schedule
+
+
+def warmup_schedule(init_lr, end_lr, warmup_steps, schedule):
+    warmup_schedule = optax.linear_schedule(
+        init_value=init_lr, end_value=end_lr, transition_steps=warmup_steps
+    )
+
+    schedule = optax.join_schedules(
+        schedules=[warmup_schedule, schedule], boundaries=[warmup_steps]
+    )
 
     return schedule
